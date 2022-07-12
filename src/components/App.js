b82e38e82fee1cddb14f3b8d5d83418fc245d54b
KamyabTabani <k.tabani82@gmail.com>
2022-07-12 11:44:32 +0430

    Structure

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,21 @@ import AboutUs from "./AboutUs";
 import FeatureSection from "./FeatureSection";
 
 const App = () => {
-    return (
-        <div className="app">
-            {/*<div className={"circle"}>
+  return (
+    <div className="app">
+      {/*<div className={"circle"}>
             </div>
             <div className={"app__inside"}>
             </div>*/}
 
-
-            <TopBar/>
-            <Header/>
-            <AboutUs/>
-            <FeatureSection/>
-        </div>
-    );
-}
+      <div>
+        <TopBar />
+        <Header />
+        <AboutUs />
+        <FeatureSection />
+      </div>
+    </div>
+  );
+};
 
 export default App;
